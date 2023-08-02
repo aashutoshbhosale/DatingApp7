456fe5783e4e89b21fb8aac1605890fb54d30f36
aashutoshbhosale <aashutoshbhosale@gmail.com>
2023-08-02 13:17:51 +0530

    End of section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {BsDropdownModule} from 'ngx-bootstrap/dropdown'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
 import{ HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,19 +8,25 @@ import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    NavComponent, HomeComponent, RegisterComponent
-  ],
+    NavComponent, HomeComponent, RegisterComponent, MemberListComponent, MemberDetailComponent, ListsComponent, MessagesComponent
+  ],  
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
